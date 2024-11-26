8bed4829d7b9009734da5b27d7c9b54384edc39a
Mahima Shawrikar <lrimsh@egmont.com>
2024-11-26 14:57:22 +0100

    changes for form to input 2 properties

--- README.md
+++ README.md
@@ -1,21 +1,20 @@
 To visit form on Vercel:
 
- 1. Go to vercel.com
- 2. Login to the Vercel account running the app (drift@xxxxxx.xx, ask Jonas)
- 3. Go to 'Deployment' tab
- 4. Click 'Visit' button
- 
-To login to the form: 
- 1. Enter username (see environment variables in Vercel)
- 2. Enter password (see environment variables in Vercel)
+1.  Go to vercel.com
+2.  Login to the Vercel account running the app (drift@xxxxxx.xx, ask Jonas)
+3.  Go to 'Deployment' tab
+4.  Click 'Visit' button
+
+To login to the form:
+
+1.  Enter username (see environment variables in Vercel)
+2.  Enter password (see environment variables in Vercel)
 
 To run it locally:
- 1. Clone the repository
- 2. Run `npm install`
- 3. Copy the `.env.example` file to `.env` and fill in the environment variables 
- 4. Run `npm run start`
- 5. Open `http://localhost:3000` in your browser
 
-
-
-
+1.  Clone the repository
+    git clone https://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@github.com/lruconfigurationservice/configurationinterface.git
+2.  Run `npm install`
+3.  Copy the `.env.example` file to `.env` and fill in username, password(anything) and other the environment variables
+4.  Run `npm run dev`
+5.  Open `http://localhost:3000` in your browser
--- src/app/UINextApi/Form.tsx
+++ src/app/UINextApi/Form.tsx
@@ -1,80 +1,82 @@
-'use client'
-import _ from 'lodash';
-import { useEffect, useState } from 'react';
-import DropdownToForm from './DropdownToForm';
-import { IFormProps } from './FormProps.interfaces';
-import { InputAsNumber, InputAsString } from './InputToForm';
-import { getRequest, putRequest } from '../utils/requests';
-import SubmitFormModal from './SubmitFormModal';
+"use client";
+import _ from "lodash";
+import { useEffect, useState } from "react";
+import DropdownToForm from "./DropdownToForm";
+import { IFormProps } from "./FormProps.interfaces";
+import { InputAsNumber, InputAsString } from "./InputToForm";
+import { getRequest, putRequest } from "../utils/requests";
+import SubmitFormModal from "./SubmitFormModal";
 
 const Form = () => {
-  const [modal, setModal] = useState(false)
+  const [modal, setModal] = useState(false);
   const toggleModal = () => {
-    setModal(!modal)
-  }
+    setModal(!modal);
+  };
   const [formData, setFormData] = useState<IFormProps>({
     publisher: {
-      heading: '',
+      heading: "",
       logo: {
-        id: '',
-        type: '',
+        id: "",
+        type: "",
       },
     },
 
     favicon: {
-      icoUrl: '',
-      appleTouchIconPngUrl: '',
-      size32PngUrl: '',
-      size16PngUrl: '',
-      manifestUrl: '',
-      safariPinnedTabSvgUrl: '',
-      safariPinnedTabSvgColor: '',
-      themeColor: '',
+      icoUrl: "",
+      appleTouchIconPngUrl: "",
+      size32PngUrl: "",
+      size16PngUrl: "",
+      manifestUrl: "",
+      safariPinnedTabSvgUrl: "",
+      safariPinnedTabSvgColor: "",
+      themeColor: "",
     },
     minutesPerLesson: 0,
-    useLessonDuration: '',
-    useLessonDurationCourse: '',
-    useLessonDurationActivity: '',
-    contactPhoneNumber: '',
+    useLessonDuration: "",
+    useLessonDurationCourse: "",
+    useLessonDurationActivity: "",
+    contactPhoneNumber: "",
     loginModalLink: {
-      heading: '',
-      url: '',
+      heading: "",
+      url: "",
     },
     licenseLink: {
-      heading: '',
-      url: '',
+      heading: "",
+      url: "",
     },
   });
 
+  const path = process.env.NEXT_PUBLIC_PATH;
+  const appId = process.env.NEXT_PUBLIC_APPID;
+  const endpoint = path + appId;
+  const apiRoot = process.env.NEXT_PUBLIC_API_ROOT_WITHOUT_VERSION;
 
-  const path = process.env.NEXT_PUBLIC_PATH
-  const appId = process.env.NEXT_PUBLIC_APPID
-  const endpoint = path+appId
-  const apiRoot = process.env.NEXT_PUBLIC_API_ROOT_WITHOUT_VERSION 
-
-  useEffect(() => {   
+  useEffect(() => {
     const fetchData = async () => {
       try {
-        const data = await getRequest<IFormProps>(
-         apiRoot, 
-          endpoint, 
-        );
-             
+        const data = await getRequest<IFormProps>(apiRoot, endpoint);
+
         setFormData({
           ...data,
-          useLessonDuration: data.useLessonDuration ? 'true' : 'false',
-          useLessonDurationActivity: data.useLessonDurationActivity ? 'true' : 'false',
-          useLessonDurationCourse: data.useLessonDurationCourse ? 'true' : 'false',
+          useLessonDuration: data.useLessonDuration ? "true" : "false",
+          useLessonDurationActivity: data.useLessonDurationActivity
+            ? "true"
+            : "false",
+          useLessonDurationCourse: data.useLessonDurationCourse
+            ? "true"
+            : "false",
         });
       } catch (error) {
-        console.log('Error fetching data ', error);
+        console.log("Error fetching data ", error);
       }
     };
 
     fetchData();
   }, [apiRoot, path, endpoint]);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
+  ) => {
     const { name, value } = e.target;
     setFormData((prevData) => _.set({ ...prevData }, name, value));
   };
@@ -82,13 +84,8 @@ const Form = () => {
   const handleSubmit = async (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-      const response = await putRequest(
-        apiRoot,
-        endpoint,
-        formData
-      
-      );
-      setModal(true)
+      const response = await putRequest(apiRoot, endpoint, formData);
+      setModal(true);
       console.log(response);
     } catch (error) {
       console.error(error);
@@ -96,206 +93,208 @@ const Form = () => {
   };
 
   return (
-    <div className='min-h-screen'>
-    {modal && (<SubmitFormModal toggleModal={toggleModal}/>)}
-    <form onSubmit={handleSubmit}>
-      <div className="grid grid-cols-3 justify-center gap-6">
-        <div className="col-span-1 p-2">
-          <h1 className="text-xl font-bold">Publisher</h1>
-          <InputAsString
-            htmlFor="heading"
-            onChange={handleChange}
-            value={formData.publisher.heading}
-            name="publisher.heading"
-            title="Heading"
-          />
-          <h2 className="text-lg font-bold">Logo</h2>
-          <InputAsString
-            htmlFor="id"
-            onChange={handleChange}
-            value={formData.publisher.logo.id}
-            name="publisher.logo.id"
-            title="Id"
-          />
-          <br />
-          <InputAsString
-            htmlFor="type"
-            onChange={handleChange}
-            value={formData.publisher.logo.type}
-            name="publisher.logo.type"
-            title="Type"
-          />
+    <div className="min-h-screen">
+      {modal && <SubmitFormModal toggleModal={toggleModal} />}
+      <form onSubmit={handleSubmit}>
+        <div className="grid grid-cols-3 justify-center gap-6">
+          <div className="col-span-1 p-2">
+            <h1 className="text-xl font-bold">Publisher</h1>
+            <InputAsString
+              htmlFor="heading"
+              onChange={handleChange}
+              value={formData.publisher.heading}
+              name="publisher.heading"
+              title="Heading"
+            />
+            <h2 className="text-lg font-bold">Logo</h2>
+            <InputAsString
+              htmlFor="id"
+              onChange={handleChange}
+              value={formData.publisher.logo.id}
+              name="publisher.logo.id"
+              title="Id"
+            />
+            <br />
+            <InputAsString
+              htmlFor="type"
+              onChange={handleChange}
+              value={formData.publisher.logo.type}
+              name="publisher.logo.type"
+              title="Type"
+            />
 
-          <br />
-          <br />
-          <br />
+            <br />
+            <br />
+            <br />
 
-          <h1 className="text-xl font-bold">Login Modal Link</h1>
-          <InputAsString
-            htmlFor="heading"
-            onChange={handleChange}
-            value={formData.loginModalLink.heading}
-            name="loginModalLink.heading"
-            title="Heading"
-          />
+            <h1 className="text-xl font-bold">Login Modal Link</h1>
+            <InputAsString
+              htmlFor="heading"
+              onChange={handleChange}
+              value={formData.loginModalLink.heading}
+              name="loginModalLink.heading"
+              title="Heading"
+            />
 
-          <br />
-          <InputAsString
-            htmlFor="url"
-            onChange={handleChange}
-            value={formData.loginModalLink.url}
-            name="loginModalLink.url"
-            title="Url"
-          />
+            <br />
+            <InputAsString
+              htmlFor="url"
+              onChange={handleChange}
+              value={formData.loginModalLink.url}
+              name="loginModalLink.url"
+              title="Url"
+            />
 
-          <br />
-          <br />
-          <br />
-          <h1 className="text-xl font-bold">License Link</h1>
-          <InputAsString
-            htmlFor="heading"
-            onChange={handleChange}
-            value={formData.licenseLink.heading}
-            name="licenseLink.heading"
-            title="Heading"
-          />
+            <br />
+            <br />
+            <br />
+            <h1 className="text-xl font-bold">License Link</h1>
+            <InputAsString
+              htmlFor="heading"
+              onChange={handleChange}
+              value={formData.licenseLink.heading}
+              name="licenseLink.heading"
+              title="Heading"
+            />
 
-          <br />
-          <InputAsString
-            htmlFor="url"
-            onChange={handleChange}
-            value={formData.licenseLink.url}
-            name="licenseLink.url"
-            title="Url"
-          />
+            <br />
+            <InputAsString
+              htmlFor="url"
+              onChange={handleChange}
+              value={formData.licenseLink.url}
+              name="licenseLink.url"
+              title="Url"
+            />
+          </div>
+
+          <div className="col-span-1 p-2">
+            <h1 className="text-xl font-bold">FavIcon</h1>
+
+            <InputAsString
+              htmlFor="icoUrl"
+              onChange={handleChange}
+              value={formData.favicon.icoUrl}
+              name="favicon.icoUrl"
+              title="Ico Url"
+            />
+            <br />
+
+            <InputAsString
+              htmlFor="appleTouchIconPngUrl"
+              onChange={handleChange}
+              value={formData.favicon.appleTouchIconPngUrl}
+              name="favicon.appleTouchIconPngUrl"
+              title="Apple Touch Icon Png Url"
+            />
+            <br />
+            <InputAsString
+              htmlFor="size32PngUrl"
+              onChange={handleChange}
+              value={formData.favicon.size32PngUrl}
+              name="favicon.size32PngUrl"
+              title="32 PNG Url"
+            />
+
+            <br />
+            <InputAsString
+              htmlFor="size16PngUrl"
+              onChange={handleChange}
+              value={formData.favicon.size16PngUrl}
+              name="favicon.size16PngUrl"
+              title="16 PNG Url"
+            />
+
+            <br />
+            <InputAsString
+              htmlFor="manifestUrl"
+              onChange={handleChange}
+              value={formData.favicon.manifestUrl}
+              name="favicon.manifestUrl"
+              title="Manifest Url"
+            />
+            <br />
+            <InputAsString
+              htmlFor="safariPinnedTabSvgUrl"
+              onChange={handleChange}
+              value={formData.favicon.safariPinnedTabSvgUrl}
+              name="favicon.safariPinnedTabSvgUrl"
+              title="Safari Pinned Tab Svg Url"
+            />
+
+            <br />
+
+            <InputAsString
+              htmlFor="safariPinnedTabSvgColor"
+              onChange={handleChange}
+              value={formData.favicon.safariPinnedTabSvgColor}
+              name="favicon.safariPinnedTabSvgColor"
+              title="Safari Pinned Tab Svg Color"
+            />
+
+            <br />
+
+            <InputAsString
+              htmlFor="themeColor"
+              onChange={handleChange}
+              value={formData.favicon.themeColor}
+              name="favicon.themeColor"
+              title="Theme Color"
+            />
+          </div>
+
+          <div className="col-span-1 p-2">
+            <h1 className="text-xl font-bold">Other</h1>
+
+            <InputAsNumber
+              htmlFor="minutesPerLesson"
+              onChange={handleChange}
+              value={formData.minutesPerLesson}
+              name="minutesPerLesson"
+              title="Minutes Per Lesson"
+            />
+
+            <br />
+
+            <DropdownToForm
+              htmlFor="useLessonDuration"
+              name="useLessonDuration"
+              value={formData.useLessonDuration}
+              onChange={handleChange}
+              title="Use Lesson Duration"
+            />
+            <br />
+            <DropdownToForm
+              htmlFor="useLessonDurationCourse"
+              name="useLessonDurationCourse"
+              value={formData.useLessonDurationCourse}
+              onChange={handleChange}
+              title="Use Lesson Duration Course"
+            />
+            <br />
+            <DropdownToForm
+              htmlFor="useLessonDurationActivity"
+              name="useLessonDurationActivity"
+              value={formData.useLessonDurationActivity}
+              onChange={handleChange}
+              title="Use Lesson Duration Activity"
+            />
+            <br />
+            <InputAsString
+              htmlFor="contactPhoneNumber"
+              onChange={handleChange}
+              value={formData.contactPhoneNumber}
+              name="contactPhoneNumber"
+              title="Contact Phone Number"
+            />
+          </div>
         </div>
 
-        <div className="col-span-1 p-2">
-          <h1 className="text-xl font-bold">FavIcon</h1>
-
-          <InputAsString
-            htmlFor="icoUrl"
-            onChange={handleChange}
-            value={formData.favicon.icoUrl}
-            name="favIcon.icoUrl"
-            title="Ico Url"
-          />
-          <br />
-
-          <InputAsString
-            htmlFor="appleTouchIconPngUrl"
-            onChange={handleChange}
-            value={formData.favicon.appleTouchIconPngUrl}
-            name="appleTouchIconPngUrl"
-            title="Apple Touch Icon Png Url"
-          />
-          <br />
-          <InputAsString
-            htmlFor="size32PngUrl"
-            onChange={handleChange}
-            value={formData.favicon.size32PngUrl}
-            name="favicon.size32PngUrl"
-            title="32 PNG Url"
-          />
-
-          <br />
-          <InputAsString
-            htmlFor="size16PngUrl"
-            onChange={handleChange}
-            value={formData.favicon.size16PngUrl}
-            name="favicon.size16PngUrl"
-            title="16 PNG Url"
-          />
-
-          <br />
-          <InputAsString
-            htmlFor="manifestUrl"
-            onChange={handleChange}
-            value={formData.favicon.manifestUrl}
-            name="favicon.manifestUrl"
-            title="Manifest Url"
-          />
-          <br />
-          <InputAsString
-            htmlFor="safariPinnedTabSvgUrl"
-            onChange={handleChange}
-            value={formData.favicon.safariPinnedTabSvgUrl}
-            name="favicon.safariPinnedTabSvgUrl"
-            title="Safari Pinned Tab Svg Url"
-          />
-
-          <br />
-
-          <InputAsString
-            htmlFor="safariPinnedTabSvgColor"
-            onChange={handleChange}
-            value={formData.favicon.safariPinnedTabSvgColor}
-            name="favicon.safariPinnedTabSvgColor"
-            title="Safari Pinned Tab Svg Color"
-          />
-
-          <br />
-
-          <InputAsString
-            htmlFor="themeColor"
-            onChange={handleChange}
-            value={formData.favicon.themeColor}
-            name="favicon.themeColor"
-            title="Theme Color"
-          />
+        <div className="font flex w-full justify-center ">
+          <button className="rounded bg-black pl-2 pr-2 text-white p-1">
+            Submit
+          </button>
         </div>
-
-        <div className="col-span-1 p-2">
-          <h1 className="text-xl font-bold">Other</h1>
-
-          <InputAsNumber
-            htmlFor="minutesPerLesson"
-            onChange={handleChange}
-            value={formData.minutesPerLesson}
-            name="minutesPerLesson"
-            title="Minutes Per Lesson"
-          />
-
-          <br />
-
-          <DropdownToForm
-            htmlFor="useLessonDuration"
-            name="useLessonDuration"
-            value={formData.useLessonDuration}
-            onChange={handleChange}
-            title="Use Lesson Duration"
-          />
-          <br />
-          <DropdownToForm
-            htmlFor="useLessonDurationCourse"
-            name="useLessonDurationCourse"
-            value={formData.useLessonDurationCourse}
-            onChange={handleChange}
-            title="Use Lesson Duration Course"
-          />
-          <br />
-          <DropdownToForm
-            htmlFor="useLessonDurationActivity"
-            name="useLessonDurationActivity"
-            value={formData.useLessonDurationActivity}
-            onChange={handleChange}
-            title="Use Lesson Duration Activity"
-          />
-          <br />
-          <InputAsString
-            htmlFor="contactPhoneNumber"
-            onChange={handleChange}
-            value={formData.contactPhoneNumber}
-            name="contactPhoneNumber"
-            title="Contact Phone Number"
-          />
-        </div>
-      </div>
-
-      <div className="font flex w-full justify-center ">
-        <button className="rounded bg-black pl-2 pr-2 text-white p-1">Submit</button>
-      </div>
-    </form>
+      </form>
     </div>
   );
 };
